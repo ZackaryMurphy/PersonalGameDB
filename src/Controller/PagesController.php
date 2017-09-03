a5cf0546eff566ab90a69f2ec93118933e1365fb
zmurphy <zmurphy1125@gmail.com>
2017-09-03 09:41:26 -0500

    Fixed namespace issues

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -12,7 +12,7 @@
  * @since     0.2.9
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
-namespace App\Controller;
+namespace PersonalGames\Controller;
 
 use Cake\Core\Configure;
 use Cake\Network\Exception\ForbiddenException;
